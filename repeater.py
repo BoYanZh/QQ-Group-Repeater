49c96a397f1f92322d98a5e4ddbc8dc9441da759
zyayoung <1328410180@qq.com>
2018-08-15 16:59:42 +0800

    repeat in English

--- repeater.py
+++ repeater.py
@@ -1,5 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import requests
+import string
+import time
+import hashlib
+import json
+import random
+
+# Translator
+api_url = "http://api.fanyi.baidu.com/api/trans/vip/translate"
+my_appid = 20180815000194642
+cyber = 'KVJEzePbERXN23tH69OX'
+lower_case = list(string.ascii_lowercase)
+
+def requests_for_dst(word):
+    salt = str(time.time())[:10]
+    final_sign = str(my_appid)+word+salt+cyber
+    final_sign = hashlib.md5(final_sign.encode("utf-8")).hexdigest()
+    paramas = {
+        'q':word,
+        'from':'zh',
+        'to':'en',
+        'appid':'%s'%my_appid,
+        'salt':'%s'%salt,
+        'sign':'%s'%final_sign
+        }
+    response = requests.get(api_url, params=paramas).content
+    json_reads = json.loads(response, encoding='utf-8')
+    return json_reads['trans_result'][0]['dst']
+
+# Repeater
 GROUPS_TO_REPEAT = ['密西根学院2018学生群', '复读万岁']
 
 last_message = {}
@@ -30,9 +60,9 @@ def onQQMessage(bot, contact, member, content):
         last_message[group_name] = [content, 4]
     elif group_name in last_message.keys() and last_message[group_name][0] == content:
         last_message[group_name][1] += 1
-        if last_message[group_name][1] == 3:
+        if last_message[group_name][1] == 2:
             if content != last_sent:
                 last_sent = content
-                bot.SendTo(contact, content)
+                bot.SendTo(contact, requests_for_dst(content))
     else:
         last_message[group_name] = [content, 1]
