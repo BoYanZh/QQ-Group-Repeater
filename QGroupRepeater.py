800e084717d1d8840a631bca00bd7e8b29953691
zyayoung <1328410180@qq.com>
2018-08-16 17:18:27 +0800

    review & settings

--- QGroupRepeater.py
+++ QGroupRepeater.py
@@ -2,21 +2,28 @@
 import random
 import time
 import re
+import logging
+import json
+import os
+from urllib2 import Request, urlopen
+from urllib import urlencode
 
 class QGroupBot:
-
-    XM_PR = 0.8
-    NOT_XM_PR = 0.1
-    RND_REPEAT_PR = 0.03
-    RND_XM_PR = 0.05
-    KW_REPEAT_PR = 1
-    MIN_MSG_INVL = 5
-    MAX_RND_RE_LEN = 20
-    MAX_RND_XM_LEN = 16
-    SLEEP_TIME = 0.5
+    JSON_LOCATION = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'settings.json')
+    SETTINGS = json.load(open(JSON_LOCATION))
+    XM_PR = SETTINGS['XM_PR']
+    NOT_XM_PR = SETTINGS['NOT_XM_PR']
+    RND_REPEAT_PR = SETTINGS['RND_REPEAT_PR']
+    RND_XM_PR = SETTINGS['RND_XM_PR']
+    KW_REPEAT_PR = SETTINGS['KW_REPEAT_PR']
+    MIN_MSG_INVL = SETTINGS['MIN_MSG_INVL']
+    MAX_RND_RE_LEN = SETTINGS['MAX_RND_RE_LEN']
+    MAX_RND_XM_LEN = SETTINGS['MAX_RND_XM_LEN']
+    SLEEP_TIME = SETTINGS['SLEEP_TIME']
     FIXED_REPLY_DICT = { \
         'AT': ['guna，别烦我', '为什么要召唤我？'], \
         'LewdDream': ['你  说  话  带  空  格', '唐  突  空  格', '要  素  察  觉'], \
+        'LewdDream_old': ['你 说 话 带 空 格', '唐 突 空 格', '要 素 察 觉'], \
         'Philosophy': ['BOY♂NEXT♂DOOR', 'DEEP♂DARK♂FANTASY', 'ASS♂WE♂CAN', 'Do you like WHAT♂YOU♂SEE', 'SLAVES GET YOUR ASS♂ BACK HERE♂', 'FA♂Q'] \
     }
 
@@ -32,6 +39,8 @@ class QGroupBot:
         self.beginTimeStamp = 0
         self.res = ''
         self.msg = ''
+
+        logging.info(os.getcwd())
 
     def responseMsg(self, msg):
         self.beginTimeStamp = time.time()
@@ -55,6 +64,7 @@ class QGroupBot:
         self.checkKeywords()
         self.checkMeme()
         self.followRepeat()
+        self.verify()
         self.rndRepeat()
         self.rndXM()
         return
@@ -62,7 +72,7 @@ class QGroupBot:
     #回复艾特
     def replyAT(self):
         if(len(self.res) == 0):
-            if(re.search(r'\[CQ:at,qq=2279711715\]', self.msg)):
+            if(re.search(r'\[CQ:at,qq='+QGroupBot.SETTINGS['QQ']+r'\]', self.msg)):
                 self.res = random.choice(QGroupBot.FIXED_REPLY_DICT['AT'])
         return
 
@@ -112,6 +122,37 @@ class QGroupBot:
         self.lastMsgInvl += 1
         return
 
+    def verify(self):
+        if (len(self.res) == 0):
+            api_url = QGroupBot.SETTINGS['review_api_url']
+            param = {
+                'access_token': QGroupBot.SETTINGS['review_access_token'],
+                'content': self.msg.decode('gbk').encode('utf-8')
+            }
+            param = urlencode(param).encode('utf-8')
+            labels_type = {
+                1: '暴恐违禁',
+                2: '文本色情',
+                3: '政治敏感',
+                4: '恶意推广',
+                5: '低俗辱骂',
+                6: '低质灌水',
+            }
+            response = Request(api_url.encode('utf-8'), data=param)
+            response = urlopen(response).read().decode('utf-8').encode('gbk')
+            for error in json.loads(response, encoding='gbk')['result']['reject']:
+                for hit in error['hit']:
+                    self.res = self.res + hit + ' '
+                self.res = self.res + ' 涉嫌' + labels_type[error['label']] + '\n'
+            #     for error in json.loads(response)['result']['review']:
+            #         for hit in error['hit']:
+            #             ret = ret + hit
+            #         ret = ret + ' 涉嫌' + labels_type[error['label']] + '\n'
+            if len(self.res) > 0:
+                self.res = ('咳咳\n'+self.res[:-1]).encode('gbk')
+            logging.info(self.res)
+        return
+
     #随机复读
     def rndRepeat(self):
         if(len(self.res) == 0):
@@ -125,7 +166,7 @@ class QGroupBot:
     #随机羡慕
     def rndXM(self):
         if(len(self.res) == 0):
-            if(len(self.msg) > 2 and not re.search(r'^xm|^羡慕|?$|？$', self.msg)):
+            if(len(self.msg) > 2 and not re.search(r'^xm|^羡慕|\?$|？$', self.msg)):
                 if(self.lastMsgInvl > QGroupBot.MIN_MSG_INVL and len(self.msg) <= QGroupBot.MAX_RND_XM_LEN):
                     myrand = random.random()
                     if(myrand <= QGroupBot.RND_XM_PR):
